64bc6660b898e6c0b97fdb8e8dbbab1a9b8d3922
Seungho Lee <poream3387@gmail.com>
2021-03-15 13:26:51 +0900

    Add defaults for Profile attributes and add signal for automating Profile creation

--- cardalog/users/models.py
+++ cardalog/users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,8 +13,17 @@ class Profile(models.Model):
         (PREMIUM, "PREMIUM")
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    country = models.CharField(max_length=30, blank=False)
-    plan = models.PositiveSmallIntegerField(choices=PLAN, null=False, blank=False)
+    country = models.CharField(default='South Korea', max_length=30, blank=False)
+    plan = models.PositiveSmallIntegerField(default=1, choices=PLAN, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
