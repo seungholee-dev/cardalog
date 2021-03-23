877014f2ce73ae403981d0243c2e49d3209730ae
Seungho Lee <poream3387@gmail.com>
2021-03-24 00:23:25 +0900

    Add birthdate field in registration form

--- cardalog/users/forms.py
+++ cardalog/users/forms.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import models
 from django import forms
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    # + country
+    
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2'] 
+        fields = ['username', 'first_name', 'last_name', 'email', 'birth_date', 'password1', 'password2']
--- cardalog/users/models.py
+++ cardalog/users/models.py
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     BASIC = 1
     PREMIUM = 2
+    
     PLAN = (
         (BASIC, "Basic"),
         (PREMIUM, "PREMIUM")
