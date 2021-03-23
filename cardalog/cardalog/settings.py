2777ff917cb8ebc2a0bd367ebdda663918182737
Seungho Lee <poream3387@gmail.com>
2021-03-23 12:23:38 +0900

    Create new Static path and link home.css

--- cardalog/cardalog/settings.py
+++ cardalog/cardalog/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom Static files DIR
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
