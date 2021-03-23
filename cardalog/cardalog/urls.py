08841a378b6c0689cb4a70533a1fe7132670ac8e
Seungho Lee <poream3387@gmail.com>
2021-03-23 23:08:08 +0900

    Add auth views to users app

--- cardalog/cardalog/urls.py
+++ cardalog/cardalog/urls.py
@@ -19,5 +19,6 @@ from core.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('users/', include('users.urls')),
 ]
