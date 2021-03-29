88fc6488e44a63f21e7fb237f2f5a1f64d615745
Seungho Lee <poream3387@gmail.com>
2021-03-29 10:45:41 +0900

    Add logout url

--- cardalog/users/urls.py
+++ cardalog/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from .views import SignUpView, MyLoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', SignUpView.as_view(template_name='registration/register.html'), name='register'),
     path('login/', MyLoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', .as_view(template_name='registration/login.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
