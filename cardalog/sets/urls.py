0cce0bde6d6a9246375eda41727901a1ca8aea5f
Seungho Lee <poream3387@gmail.com>
2021-04-27 15:42:58 +0900

    Implement set detail view

--- cardalog/sets/urls.py
+++ cardalog/sets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from sets.views import SetListView
+from sets.views import SetListView, SetDetailView
 
 urlpatterns = [
     path('list/', SetListView.as_view(), name='set-list'),
+    path('detail/<int:pk>', SetDetailView.as_view(), name='set-detail'),
 ]
--- cardalog/sets/views.py
+++ cardalog/sets/views.py
@@ -1,8 +1,13 @@
 from sets.models import Set
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 class SetListView(ListView):
     model = Set
     template_name = "set_list.html"
     context_object_name = 'sets'
+
+class SetDetailView(DetailView):
+    model = Set
+    template_name = "set_detail.html"
