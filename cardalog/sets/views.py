64645c69d42f85bec474e32df28508a57f79754a
Seungho Lee <poream3387@gmail.com>
2021-04-30 17:43:49 +0900

    add reverse_lazy to success_url

--- cardalog/sets/views.py
+++ cardalog/sets/views.py
@@ -1,8 +1,9 @@
+from django.urls import reverse_lazy
 from sets.models import Set
 from cards.models import Card
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 
 # Create your views here.
 class SetListView(ListView):
@@ -17,9 +18,15 @@ class SetDetailView(DetailView):
 class SetCreateView(CreateView):
     model = Set
     template_name = "set_create.html"
-    success_url = '/sets/list/' # Where to direct once the form is valid
+    success_url = reverse_lazy('set-list') # Where to direct once the form is valid
     fields = ['title', 'description', 'category', 'is_public']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+# class SetDeleteView(DeleteView):
+#     model = Set
+#     success_url = '/sets/list/'
+    
+    
