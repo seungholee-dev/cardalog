c10cf24a779cb0c815d68bda2a858486dde16ed4
Seungho Lee <poream3387@gmail.com>
2021-04-30 16:01:00 +0900

    Implement SetCreateView

--- cardalog/sets/urls.py
+++ cardalog/sets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from sets.views import SetListView, SetDetailView
+from sets.views import SetListView, SetDetailView, SetCreateView
 
 urlpatterns = [
     path('list/', SetListView.as_view(), name='set-list'),
     path('detail/<int:pk>', SetDetailView.as_view(), name='set-detail'),
+    path('create/', SetCreateView.as_view(), name='set-create'),
 ]
--- cardalog/sets/views.py
+++ cardalog/sets/views.py
@@ -2,6 +2,7 @@ from sets.models import Set
 from cards.models import Card
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 class SetListView(ListView):
@@ -13,7 +14,12 @@ class SetDetailView(DetailView):
     model = Set
     template_name = "set_detail.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['card_list'] = self.card_set.all()
-    #     return context
+class SetCreateView(CreateView):
+    model = Set
+    template_name = "set_create.html"
+    success_url = '/sets/list/' # Where to direct once the form is valid
+    fields = ['title', 'description', 'category', 'is_public']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
