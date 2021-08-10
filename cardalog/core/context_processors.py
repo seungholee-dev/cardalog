3ce8c0017df468fd748d70882e4394340feccd06
Seungho Lee <poream3387@gmail.com>
2021-08-10 16:39:06 +0900

    Add Notification icon in nav bar

--- cardalog/core/context_processors.py
+++ cardalog/core/context_processors.py
@@ -1,6 +1,6 @@
 from users.forms import UserRegisterForm
 
-# Adds UserRegisterForm for the Base template(index.html).
+# Adds UserRegisterForm for the Base template(index.html). -> Below Context Variables will be able to be used across the any app actually.
 # Context Processor
 def add_variable_to_context(request):
     return {
