0927fc77d7bcdda6a8a3a66f006fc361c9e92826
Seungho <poream3387@gmail.com>
2021-12-05 16:11:46 +0900

    Replace J Bootstrap Jquery with other JQuery to make it work

--- cardalog/users/static/js/login.js
+++ cardalog/users/static/js/login.js
@@ -1,28 +1,50 @@
-function onClickRegister() {
-}
 
-// Jquery for Login Modal (AJAX)
-$('#authModal').ready(function(){
-    console.log('it worked');
+// Reset the Modal when it closes
+$('#authModal').on('hide.bs.modal', function () {
+    $(this).find()
+})
+
+// Login Modal (AJAX) Login request
+$("#authModal").ready(function () {
+
     // On form submit, do ajax instead
-    $("#login-form").submit(function(e){
-        var loginUrl = $(this).attr('action');
+    $("#login-form").submit(function (e) {
+        var loginUrl = $(this).attr("action");
         var loginData = $(this).serialize();
-        console.log(loginData)
-        $.post(loginUrl, loginData).done(function(data){
-            if (data.status == 'ok'){
+        console.log(loginData);
+        $.post(loginUrl, loginData).done(function (data) {
+            if (data.status == "ok") {
                 // print "success" on the html somewhere
                 console.log(data.status);
-                
+
                 // Refresh the page
                 window.location.reload();
-            // Something iss wrong with the credential
+                // Something iss wrong with the credential
             } else {
                 console.log(data.status);
-                $('#login-error-text').css('visibility', 'visible');
+                $("#login-error-text").css("visibility", "visible");
             }
         });
         e.preventDefault(); // This stops page reload
     });
-});
 
+    // Initial visibility set ups for Login / SignUp Modals
+    $("#signUpModal").hide();
+    $("#resetModal").hide();
+
+    // Toggle Sign up text
+    $("#toggleSignUp").click(function (e) {
+        $("#loginModal").hide();
+        $("#signUpModal").show();
+    });
+
+    // Toggle Reset Email text
+    $("#toggleReset").click(function (e) {
+        $("#loginModal").hide();
+        $("#resetModal").show();
+    });
+
+    $("#resetButton").click(function (e) {
+        $("#resetButton").text("Return to Login");
+    })
+});
