dbe09adf0a736de795164835d0e6e16a874cfe59
Seungho <poream3387@gmail.com>
2021-12-05 13:13:24 +0900

    Extract Login modal code and combine login modal with sign up modal

--- cardalog/static/js/index.js
+++ cardalog/static/js/index.js
@@ -5,33 +5,6 @@ setTimeout(function() {
    flashMessage.style.display = "none"; 
 }, 3000);
 
-// Jquery for Login Modal (AJAX)
-// With your other jQuery, or in some other script
-$('#loginModal').ready(function(){
-    console.log('it worked');
-    // On form submit, do ajax instead
-    $("#login-form").submit(function(e){
-        var loginUrl = $(this).attr('action');
-        var loginData = $(this).serialize();
-        $.post(loginUrl, loginData).done(function(data){
-
-            if (data.status == 'ok'){
-            // print "success" on the html somewhere
-                console.log(data.status);
-                
-                // Refresh the page
-                window.location.reload();
-
-            // Something is wrong with the credential
-            } else {
-                console.log(data.status);
-                $('#login-error-text').css('visibility', 'visible');
-            }
-        });
-        e.preventDefault(); // This stops page reload
-    });
-});
-
 // Delete confirm for Global (Used in Set)
 function deleteConfirmPopUp() {
     let result = confirm('Are you sure you want to delete?');
---
+++ cardalog/users/static/js/login.js
@@ -0,0 +1,27 @@
+function onClickRegister() {
+}
+
+// Jquery for Login Modal (AJAX)
+$('#authModal').ready(function(){
+    console.log('it worked');
+    // On form submit, do ajax instead
+    $("#login-form").submit(function(e){
+        var loginUrl = $(this).attr('action');
+        var loginData = $(this).serialize();
+        console.log(loginData)
+        $.post(loginUrl, loginData).done(function(data){
+            if (data.status == 'ok'){
+                // print "success" on the html somewhere
+                console.log(data.status);
+                
+                // Refresh the page
+                window.location.reload();
+            // Something iss wrong with the credential
+            } else {
+                console.log(data.status);
+                $('#login-error-text').css('visibility', 'visible');
+            }
+        });
+        e.preventDefault(); // This stops page reload
+    });
+});
